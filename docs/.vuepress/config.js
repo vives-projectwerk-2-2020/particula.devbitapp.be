2b0b8c6792dcceb435549494015b8dca3ae25dad
Nico De Witte <nico.dewitte@outlook.com>
2020-02-04 20:54:52 +0100

    Setup vuepress with basic configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Particula Project Documentation',
+  description: 'Particula Project Documentation',
+  themeConfig: {
+    nav: [
+      {text: 'Redmine', link: 'https://project.devbit.be'},
+      {text: 'Organization', link: 'https://github.com/vives-projectwerk-2-2020'}
+    ],
+    sidebar: [
+      '/'
+    ],
+    sidebarDepth: 1,
+    repo: 'vives-projectwerk-2-2020/particula.devbitapp.be',
+    docsDir: 'docs',
+    docsBranch: 'master'
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  serviceWorker: true,
+  plugins: [],
+}
